50276fa7c9114b918e289360ed6f2a3dd70b2149
03y <98747393+03y@users.noreply.github.com>
2024-05-01 16:50:07 +0100

    add python port

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+
+import os
+import psutil
+import platform
+
+api = Flask(__name__)
+
+@api.route('/hello', methods=['GET'])
+def hello():
+    return 'hello world'
+
+@api.route('/uptime', methods=['GET'])
+def uptime():
+	output = os.popen('uptime').read()
+	return output
+
+@api.route('/mem', methods=['GET'])
+def mem():
+	output = psutil.virtual_memory().percent
+	print(output)
+	return str(output)
+
+if __name__ == '__main__':
+    api.run()
+
